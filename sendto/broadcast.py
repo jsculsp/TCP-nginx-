d1f271bb93e5285d66428ecc2d15bd7759de459d
jsculsp <328232234@qq.com>
2017-04-18 14:01:11 +0800

    save

--- sendto/broadcast.py
+++ sendto/broadcast.py
@@ -2,3 +2,25 @@ def broadcast(source, consumers):
     for item in source:
         for c in consumers:
             c.send(item)
+
+
+# Example
+if __name__ == '__main__':
+
+    class Consumer(object):
+        def __init__(self, name):
+            self.name = name
+
+        def send(self, item):
+            print self, "got", item
+
+        def __repr__(self):
+            return self.name
+
+    c1 = Consumer('consumer1')
+    c2 = Consumer('consumer2')
+    c3 = Consumer('consumer3')
+
+    from python_tail_f import follow
+    lines = follow(open("access-log"))
+    broadcast(lines, [c1, c2, c3])
---
+++ sendto/netsend.py
@@ -0,0 +1,34 @@
+import socket
+import pickle
+
+
+class NetConsumer(object):
+    def __init__(self, addr):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect(addr)
+
+    def send(self, item):
+        pitem = pickle.dumps(item)
+        self.s.sendall(pitem)
+
+    def close(self):
+        self.s.close()
+
+# Example use. This requires you to run receivefrom.py first.
+
+if __name__ == '__main__':
+    from broadcast import broadcast
+    from python_tail_f import follow
+    from nginx_log import nginx_log
+
+    # A class that sends 404 requests to another host
+    class Stat404(NetConsumer):
+        def send(self, item):
+            if item['status'] == 404:
+                super(Stat404, self).send(item)
+
+
+    stat404 = Stat404(("192.168.4.248", 3333))
+    lines = follow('/var/log/nginx/3d.access.log')
+    log = nginx_log(lines)
+    broadcast(log, [stat404])
