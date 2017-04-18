b3189ed8289544324555cc8809ccd2b1b731283d
jsculsp <328232234@qq.com>
2017-04-18 17:35:35 +0800

    save

---
+++ others/gentrace.py
@@ -0,0 +1,18 @@
+# gentrace.py
+#
+# Trace a generator by printing items received
+
+
+def trace(source):
+    for item in source:
+        print(item)
+        yield item
+
+# Example use
+if __name__ == '__main__':
+    from sendto.nginx_log import nginx_log
+    from python_tail_f import follow
+
+    lines = follow('/var/log/nginx/3d.access.log')
+    log = trace(nginx_log(lines))
+    r404 = trace(r for r in log if r['status'] == 404)
