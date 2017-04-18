f19bcda60ce97e4cc5d6d93f70173da4482e4dcc
jsculsp <328232234@qq.com>
2017-04-18 16:18:24 +0800

    save

--- sendto/consumer_thread.py
+++ sendto/consumer_thread.py
@@ -1,5 +1,6 @@
 import queue
 import threading
+from broadcast import broadcast
 
 
 class ConsumerThread(threading.Thread):
@@ -20,3 +21,27 @@ class ConsumerThread(threading.Thread):
 
     def run(self):
         self.target(self.generate())
+
+
+if __name__ == '__main__':
+    from python_tail_f import follow
+    from nginx_log import nginx_log
+
+    def find_404(log):
+        for r in (r for r in log if r['status'] == 404):
+            print('[{}] [{}] {}'.format(r['status'], r['datetime'], r['request']))
+
+    def bytes_transferred(log):
+        total = 0
+        for r in log:
+            total += r['bytes']
+            print('Total bytes: {}'.format(total))
+
+    c1 = ConsumerThread(find_404)
+    c1.start()
+    c2 = ConsumerThread(bytes_transferred)
+    c2.start()
+
+    lines = follow('/var/log/nginx/3d.access.log')
+    log = nginx_log(lines)
+    broadcast(log, [c1, c2])
